c7cf2b24f7d0a81ab374ae53a84fafb50ad8b989
Matthew Blanski <mblanski@salesforce.com>
2022-02-25 21:14:32 -0600

    Added heavily debugged code

--- force-app/main/campusAlert/lwc/campaignFacilityEventVisualizer/campaignFacilityEventVisualizer.js
+++ force-app/main/campusAlert/lwc/campaignFacilityEventVisualizer/campaignFacilityEventVisualizer.js
@@ -16,11 +16,16 @@ export default class CampaignFacilityEventVisualizer extends LightningElement {
     @api recordId;
 
     facilityModels;
-    slackChannels;
+    slackChannels = [];
     contactIds = [];
+    facilityContactCounts = {};
 
     treeGridColumns = TREE_GRID_COLUMNS;
     treeGridData;
+
+    get stringifiedContactIds() {
+        return JSON.stringify(this.contactIds);
+    }
 
     connectedCallback() {
         getFacilityModels({ campaignId: this.recordId })
@@ -55,12 +60,51 @@ export default class CampaignFacilityEventVisualizer extends LightningElement {
         Promise.all(contactPromises)
         .then((result) => {
             console.log(JSON.stringify(result));
+            this.processFacilityResults(result);
+            console.log('Facility Result parsing complete')
         })
         .catch((error) => {
             console.log(error);
         })
         .finally(() => {
-            console.log('Processing Complete');
+            console.log('Promise Resolution Complete');
         });
+    }
+
+    processFacilityResults(facilityResults) {
+        facilityResults.forEach((facilityReturn) => {
+            console.log('Facility info, id, channels, contacts')
+            console.log(facilityReturn.facilityId);
+            console.log(facilityReturn.slackChannels);
+            console.log(facilityReturn.contactIds);
+
+            //Add slack channels to map
+            this.slackChannels.concat(facilityReturn.slackChannels);
+            this.contactIds.concat(facilityReturn.contactIds);
+
+
+            if(!this.facilityContactCounts[facilityReturn.facilityId]) {
+                this.facilityContactCounts[facilityReturn.facilityId] = facilityReturn.length;
+                console.log('Making new facility entry');
+                console.log(this.facilityContactCounts[facilityReturn.facilityId]);
+            } else {
+                console.log('Found old facility entry');
+                this.facilityContactCounts[facilityReturn.facilityId] = this.facilityContactCounts[facilityReturn.facilityId] + facilityReturn.length;
+                console.log(this.facilityContactCounts[facilityReturn.facilityId]);
+            }
+            console.log('Facility and Contact Counts');
+            console.log(JSON.stringify(facilityReturn.contactIds));
+            console.log('Facility contact counts');
+            console.log(JSON.stringify(this.facilityContactCounts));
+            console.log('Contact counts');
+            console.log(this.contactIds);*/
+        });
+
+        //Filter slack channels for uniqueness
+        this.slackChannels = [...new Map(this.slackChannels.map(slackChannel => [slackChannel, TRUE])).keys()];
+        //Filter contacts for uniqueness
+        this.contactIds = [...new Map(this.contactIds.map(contactId => [contactId, TRUE])).keys()];
+        console.log('Contacts');
+        console.log(JSON.stringify(this.contactIds));
     }
 }
